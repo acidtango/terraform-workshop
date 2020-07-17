4dc8a8b606070a08570fd3b121ec5d600d4b11ac
Aaron Perez <aarperper@gmail.com>
2020-07-17 15:22:08 +0100

    Adds pubsub resource for the bucket

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "gcs" {
     credentials = "./service_account.json"
-    bucket  = "terraform-workshop-states"
-    prefix  = "terraform/state"
+    bucket      = "terraform-workshop-states"
+    prefix      = "terraform/state"
   }
 }
 
@@ -23,4 +23,28 @@ resource "google_storage_bucket" "bucket_gustavo" {
   }
 
   bucket_policy_only = true
+}
+
+resource "google_storage_notification" "notification" {
+  bucket         = google_storage_bucket.bucket_gustavo.name
+  payload_format = "JSON_API_V1"
+  topic          = google_pubsub_topic.topic.id
+  event_types    = ["OBJECT_FINALIZE", "OBJECT_METADATA_UPDATE"]
+  custom_attributes = {
+    new-attribute = "new-attribute-value"
+  }
+  depends_on = [google_pubsub_topic_iam_binding.binding]
+}
+
+resource "google_pubsub_topic_iam_binding" "binding" {
+  topic   = google_pubsub_topic.topic.id
+  role    = "roles/pubsub.publisher"
+  members = ["serviceAccount:${data.google_storage_project_service_account.gcs_account.email_address}"]
+}
+
+data "google_storage_project_service_account" "gcs_account" {
+}
+
+resource "google_pubsub_topic" "topic" {
+  name = "default_topic"
 }
