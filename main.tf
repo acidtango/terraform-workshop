6c6bd66f3470923ffad6368665e428f5c6f94435
Gustavo Gutierrez <gustavo@acidtango.com>
2020-07-17 15:43:22 +0200

    task: initialized gcs backend

--- main.tf
+++ main.tf
@@ -1,5 +1,21 @@
+terraform {
+  backend "gcs" {
+    credentials = "./service_account.json"
+    bucket  = "terraform-workshop-states"
+    prefix  = "terraform/state"
+  }
+}
+
 provider "google" {
   credentials = file("./service_account.json")
   project     = "terraform-workshop-283612"
   region      = "europe-west2"
+}
+
+resource "google_storage_bucket" "bucket_gustavo" {
+  name          = "my-test-bucket-for-gustavo"
+  location      = "EU"
+  force_destroy = true
+
+  bucket_policy_only = true
 }
